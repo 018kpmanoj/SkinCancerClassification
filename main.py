6d14bf8e1937fb9c246724d03b069cea2033b93e
Karan <karanlvm123@gmail.com>
2022-04-10 01:41:21 +0530

    SERes model

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 import os
+import torch
+import numpy as np
+import pandas as pd
+import pretrainedmodels
+import torch.nn as nn
+
+from torch.nn import functional as F
+
+#Area under curve 
+
+
+class SEResNext50_32x4d(nn.Module):
+    def __init__(self, pretrained="imagenet"):
+        super(SEResNext50_32x4d, self).__init__()
+        self.model = pretrainedmodels.__dict__["se_resnext50_32x4d"](pretrained=pretrained)
+        self.out = nn.Linear(2048, 1)
+    def forward(self, image):
+        bs, _, _, _ = image.shape
+        x = self.model.features(image)
+        x = F.adaptive_avg_pool2d(x, 1)
+        x = x.reshape(bs, -1)
+        out = self.out(x)
+        return out
+ 
