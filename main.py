78bd761cbf2cc4e55b06aaf8e35c9b419864acf0
Karan <karanlvm123@gmail.com>
2022-04-13 10:28:22 +0530

    Created base model

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
+from sched import scheduler
 import torch
 import numpy as np
 import pandas as pd
 import pretrainedmodels
 import torch.nn as nn
+import albumentations
 
 from torch.nn import functional as F
+
+from wtfml.data_loaders.image import ClassificationDataLoader
 
 #Area under curve 
 
@@ -23,3 +27,79 @@ class SEResNext50_32x4d(nn.Module):
         out = self.out(x)
         return out
  
+def run(fold):
+    training_data_path = "/home/karan/Desktop/Melonoma-Classifier/jpeg/resized_train"
+    df = pd.read_csv = "/home/karan/Desktop/Melonoma-Classifier/train_Folds.csv"
+    device = "cuda"
+    epochs = 50
+    train_bs = 32
+    valid_bs = 16
+
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+
+
+    df_train = df[df.kfold != fold].reset_index(drop = True)
+    df_valid = df[df.kfold == fold].reset_index(drop = True)
+
+    train_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True)
+        ]
+    )
+
+
+    valid_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True)
+        ]
+    )
+
+
+    train_images = df.train.image_name.values.tolist()
+    train_images = [os.path.join(training_data_path, i + ".jpg") for i in train_images]
+    train_targets = df_train.target.values
+
+    valid_images = df.valid.image_name.values.tolist()
+    valid_images = [os.path.join(training_data_path, i + ".jpg") for i in valid_images]
+    valid_targets = df_valid.target.values
+
+    train_dataset = ClassificationDataLoader(
+        image_paths=train_images,
+        targets=train_targets,
+        resize=None,
+        augmentations=train_aug
+    )
+
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size = train_bs,
+        shuffle = True,
+        num_workers = 4
+    )
+
+
+
+    valid_dataset = ClassificationDataLoader(
+        image_paths=valid_images,
+        targets=valid_targets,
+        resize=None,
+        augmentations=valid_aug
+    )
+
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size = valid_bs,
+        shuffle = False,
+        num_workers = 4
+    )
+
+    model = SEResNext50_32x4d(pretrained="imagenet")
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=le-4)
+    scheduler = torch.optim.le_scheduler.ReduceLROnPlateau(
+        
+    )
